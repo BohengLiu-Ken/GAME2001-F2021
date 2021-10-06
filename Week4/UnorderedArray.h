36981678b9102d475e1a7015b3e98d715fb3ee13
BohengLiu <ken953943785@yahoo.com.hk>
2021-10-06 01:00:47 -0400

    Fix Error

--- Week4/UnorderedArray.h
+++ Week4/UnorderedArray.h
@@ -302,4 +302,4 @@ private:
 	int m_maxSize;		// Maximum size of the array
 	int m_growSize;		// Amount the array can grow through expansion
 	int m_numElements;	// Number of items currently in my array
-}; #pragma once
+}; 
